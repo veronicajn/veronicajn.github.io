30fb137186de6627026ac3ec2021c04bcb9931b1
veronicajn <62155917+veronicajn@users.noreply.github.com>
2020-12-09 19:49:09 +0100

    primer intento deploy

---
+++ build/precache-manifest.6684b6095066709b22e141b0dce7fb8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3603641cfef31c7fc201d701adda647e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08153c7610bd99405058",
+    "url": "/static/css/main.5eb2e062.chunk.css"
+  },
+  {
+    "revision": "79c6e5bd83ebbb4e1710",
+    "url": "/static/js/2.507a8db3.chunk.js"
+  },
+  {
+    "revision": "a8b70e52ba7dea8d05d863a041e11c75",
+    "url": "/static/js/2.507a8db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08153c7610bd99405058",
+    "url": "/static/js/main.66809683.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
